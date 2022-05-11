8efce5e49f067c98740f0183c0dd0ed229c90cd1
SidathDabare <sidath2007@gmail.com>
2022-05-12 00:08:36 +0200

    homework

--- index.js
+++ index.js
@@ -2,20 +2,8 @@ let outerArray = [];
 
 const filterBooks = (event) => {
   const query = event.target.value;
-
   const select = document.querySelector("select");
-
   console.log(select.value);
-
-  // fetch("https://striveschool-api.herokuapp.com/books")
-  //     .then(response => response.json())
-  //     .then(books => {
-
-  //         const filteredBooks = books.filter(book => book.title.toLowerCase().includes(query.toLowerCase()))
-  //         console.log(filteredBooks)
-  //         renderBooks(filteredBooks)
-  //     })
-
   const filteredBooks = outerArray.filter(
     (book) =>
       book.title.toLowerCase().includes(query.toLowerCase()) &&
@@ -25,40 +13,24 @@ const filterBooks = (event) => {
 };
 
 const loadBooks = async () => {
-  // fetch("https://striveschool-api.herokuapp.com/books")
   try {
     const response = await fetch(
       "https://be-strive-bookstore.herokuapp.com/books"
     );
     const parsedBody = await response.json();
-
     const { data: books } = parsedBody;
-
     bookToRemoveIdx = books.findIndex((book) => book.price === null);
     books.splice(bookToRemoveIdx, 1);
-
     outerArray = [...books];
     renderBooks(books);
   } catch (err) {
     console.log(err);
   }
-
-  // fetch("https://be-strive-bookstore.herokuapp.com/books")
-  // .then(response => response.json())
-  // .then(({ data: books }) => {
-  //     bookToRemoveIdx = books.findIndex(book => book.price === null)
-  //     books.splice(bookToRemoveIdx, 1)
-
-  //     outerArray = [...books]
-  //     renderBooks(books)
-  // })
-  // .catch(err => console.log(err))
   console.log("HERE");
 };
 
 const selectCategory = (event) => {
   const option = event.target.value;
-
   const filteredBooks = outerArray.filter((book) =>
     book.category.toLowerCase().includes(option.toLowerCase())
   );
@@ -67,7 +39,6 @@ const selectCategory = (event) => {
 
 window.onload = () => {
   loadBooks();
-
   document.querySelector("input").addEventListener("change", filterBooks);
   document.querySelector("select").addEventListener("change", selectCategory);
 };
@@ -75,11 +46,10 @@ window.onload = () => {
 const renderBooks = (books) => {
   const row = document.querySelector(".row");
   row.innerHTML = "";
-
   if (Array.isArray(books)) {
-    books.forEach((book) => {
+    books.forEach((book, index) => {
       const col = document.createElement("div");
-      col.className = "col-md-3 mb-2 px-2";
+      col.className = "col-6 col-sm-4 col-md-6 col-lg-4 mb-2 px-2";
       col.innerHTML = `<div class="card">
                             <img src=${book.img} class="card-img-top" alt="...">
                             <div class="card-body pl-2">
@@ -89,48 +59,52 @@ const renderBooks = (books) => {
                                 <p class="card-text">$${book.price}</p>
 
                                 <button class="btn btn-info col-7 addToCart">ADD TO CART</button>
-                                <button class="btn btn-primary col-4 skipBtn" onclick="skipBtn()">SKIP</button>
+                                <button class="btn btn-primary col-4 skipBtn">SKIP</button>
                             </div>
                         </div>`;
-      addToCart();
-      skipBtn();
-      row.appendChild(col);
-    });
 
-    // row.innerHTML = books.map((book) => `<div class="col-md-3 mb-2 px-2">
-    //                                         <div class="card">
-    //                                             <img src=${book.img} class="card-img-top" alt="...">
-    //                                             <div class="card-body">
-    //                                                 <h5 class="card-title">${book.title}</h5>
-    //                                                 <p class="card-text">${book.category}</p>
-    //                                                 <a href="#" class="btn btn-primary">$${book.price}</a>
-    //                                             </div>
-    //                                         </div>
-    //                                     </div>`).join("")
+      row.appendChild(col);
+      addToCart(book.img, book.asin, book.price, book.title);
+      skipBtn();
+    });
   } else {
     alert("provide a valid array");
   }
 };
-let addCount = 0;
-let addToCart = () => {
+let cartBody = document.querySelector(".cart-body");
+
+console.log(cartBody);
+let addToCart = (img, asin, price, title) => {
   let hideBtn = document.querySelectorAll("button.addToCart");
-  for (let i = 0; i < hideBtn.length; i++) {
-    const button = hideBtn[i];
-    button.addEventListener("click", (e) => {
+  hideBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      let li = document.createElement("li");
+      li.classList.add("cart-item");
+
+      li.innerHTML = ` <div class="justify-content-between"><img class="cart-img" src="${img}">
+                        <h5 class="card-text font-weight-bold pt-2">$ ${price}</h5></div>
+                      <div class="card-title text-right">
+                      <h5 class="card-title">${title}</h5>
+                        <p class="card-text">id: ${asin}</p>
+                      </div>
+                    `;
+
+      cartBody.appendChild(li);
       e.target.parentElement.parentElement.parentElement.classList.add(
-        "bg-danger"
+        "bg-secondary"
       );
     });
-  }
-  addCount + 1;
+    return;
+  });
 };
 
 let skipBtn = () => {
   let skipBtn = document.querySelectorAll("button.skipBtn");
-  for (let i = 0; i < skipBtn.length; i++) {
-    const button = skipBtn[i];
-    button.addEventListener("click", (e) => {
-      e.target.parentElement.parentElement.parentElement.remove();
+  skipBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.target.parentElement.parentElement.parentElement.classList.add(
+        "d-none"
+      );
     });
-  }
+  });
 };
